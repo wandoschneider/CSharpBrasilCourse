89e5fd11aff45e79dd3a05ac3016eb6c9c62aeb2
Wando Schneider <wandoschneider@MacBook-Air-de-Wando.local>
2021-01-11 20:46:28 -0300

    Curso sobre como lidar com padrões brasileiros

---
+++ ValidadorDeDocumentos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Caelum.Stella.CSharp.Validation;
+using Caelum.Stella.CSharp.Vault;
+
+namespace ValidadorDeDocumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cpf1 = "02604042096";
+            var cpf2 = "02604042000";
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+        }
+
+        private static void ValidarCPF(string cpf)
+        {
+            try
+            {
+                new CPFValidator().AssertValid(cpf);
+                Debug.WriteLine("CPF válido: " + cpf);
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine("CPF inválido: " + cpf + " : " + exc.ToString());
+            }
+        }
+
+    }
+}
